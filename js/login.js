b22148305efe9bf51faf8fcc3311bacbc77defdc
Raghav Dhingra <raghav.dhingra15@gmail.com>
2019-02-14 20:50:07 +0530

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,5 @@
+const show = () => {
+    document.getElementByClassName('loginhead').style.opacity='0';
+    document.getElementByClassName('login').style.opacity='0';
+
+}
---
+++ js/nav.js
@@ -0,0 +1,54 @@
+const change1 = () => {
+    document.getElementById('btn1').className='topelmt active';
+    document.getElementById('btn2').className='topelmt';
+    document.getElementById('btn3').className='topelmt';
+    document.getElementById('btn4').className='topelmt';
+    document.getElementById('btn5').className='topelmt';
+    document.getElementById('btn6').className='topelmt';
+    document.getElementById('heading').innerText='Profile';
+}
+const change2 = () => {
+    document.getElementById('btn2').className='topelmt active';
+    document.getElementById('btn1').className='topelmt';
+    document.getElementById('btn3').className='topelmt';
+    document.getElementById('btn4').className='topelmt';
+    document.getElementById('btn5').className='topelmt';
+    document.getElementById('btn6').className='topelmt';
+    document.getElementById('heading').innerText='About';
+}
+const change3 = () => {
+    document.getElementById('btn3').className='topelmt active';
+    document.getElementById('btn2').className='topelmt';
+    document.getElementById('btn1').className='topelmt';
+    document.getElementById('btn4').className='topelmt';
+    document.getElementById('btn5').className='topelmt';
+    document.getElementById('btn6').className='topelmt';
+    document.getElementById('heading').innerText='Events';
+}
+const change4 = () => {
+    document.getElementById('btn4').className='topelmt active';
+    document.getElementById('btn2').className='topelmt';
+    document.getElementById('btn3').className='topelmt';
+    document.getElementById('btn1').className='topelmt';
+    document.getElementById('btn5').className='topelmt';
+    document.getElementById('btn6').className='topelmt';
+    document.getElementById('heading').innerText='Schedule';
+}
+const change5 = () => {
+    document.getElementById('btn5').className='topelmt active';
+    document.getElementById('btn2').className='topelmt';
+    document.getElementById('btn3').className='topelmt';
+    document.getElementById('btn4').className='topelmt';
+    document.getElementById('btn1').className='topelmt';
+    document.getElementById('btn6').className='topelmt';
+    document.getElementById('heading').innerText='Sponsor';
+}
+const change6 = () => {
+    document.getElementById('btn6').className='topelmt active';
+    document.getElementById('btn2').className='topelmt';
+    document.getElementById('btn3').className='topelmt';
+    document.getElementById('btn4').className='topelmt';
+    document.getElementById('btn5').className='topelmt';
+    document.getElementById('btn1').className='topelmt';
+    document.getElementById('heading').innerText='Contact';
+}
